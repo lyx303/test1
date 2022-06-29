505efbb7315c611695eb8c10a0f0ee22a09f69b3
unknown <3036096945@qq.com>
2022-06-30 02:47:56 +0800

    three

--- javase/hello-app/src/test/Test.java
+++ javase/hello-app/src/test/Test.java
@@ -2,7 +2,7 @@ package test;
 
 public class Test {
     public static void main(String[] args) {
-        int rs = add(1,2);
+        int rs = add(3,2);
 
     }
     public static int add(int a,int b){
